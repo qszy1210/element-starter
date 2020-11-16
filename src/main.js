999727d2e3ca91c68c00bb2b681746537aff39e6
北山秋叶 <asdfqs@163.com>
2020-11-16 15:34:24 +0800

    add: 增加按需加载

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
-import ElementUI from 'element-ui'
+// import ElementUI from 'element-ui'
+import { Button, Notification} from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
+Vue.use(Button)
+
+Vue.prototype.$notify = Notification;
 
 new Vue({
   el: '#app',
